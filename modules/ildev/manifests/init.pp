5fe3e3f5192e68bea54a3d8770748400b547bb4b
Marcio Mazza <mazza@interlegis.leg.br>
2013-08-01 21:09:37 +0000

    variavel de ambiente $HOME para Exec

--- modules/ildev/manifests/init.pp
+++ modules/ildev/manifests/init.pp
@@ -8,7 +8,8 @@ class ildev {
   Exec {
     path => "/usr/local/bin:/usr/bin:/usr/sbin/:/bin:/sbin",
     user => "$user",
-  }
+    environment => "HOME=$home",
+ }
 
   # comuns
   package { ['subversion', 'tree', 'xclip', ]: }
