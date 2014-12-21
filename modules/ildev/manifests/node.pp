3a8dff5f802a80a38d1a915b1b4491a4cf4ba07b
Marcio Mazza <marciomazza@gmail.com>
2014-12-21 10:45:01 -0200

    Manifest for node.js stuff

--- modules/ildev/manifests/node.pp
+++ modules/ildev/manifests/node.pp
@@ -1,4 +1,4 @@
-class ildev::recess inherits ildev {
+class ildev::node inherits ildev {
 
   exec { "ppa_chris_lea_node":
     command => "add-apt-repository --yes ppa:chris-lea/node.js && apt-get update",
@@ -9,6 +9,8 @@ class ildev::recess inherits ildev {
     ensure => latest,
     require => Exec['ppa_chris_lea_node'],
   }
+
+  # TOOLS
 
   exec { "npm install -g recess -verbose":
     user => "root",
